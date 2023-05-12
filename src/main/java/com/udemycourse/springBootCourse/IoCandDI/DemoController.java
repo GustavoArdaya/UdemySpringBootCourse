09bdfe2e1e3c5c340e8a89c3553f658a6fbaee91
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-12 11:25:12 +0200

    add security user name and password

--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/DemoController.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/DemoController.java
@@ -1,6 +1,7 @@
 package com.udemycourse.springBootCourse.IoCandDI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class DemoController {
     @Autowired  // Tells Spring to inject a dependency. Annotation is optional if you only have one constructor
     public DemoController(Coach theCoach) {
         myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
     }
 }
