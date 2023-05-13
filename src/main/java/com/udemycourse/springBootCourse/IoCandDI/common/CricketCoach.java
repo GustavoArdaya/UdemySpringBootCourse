319551390fc3e4d1dd682811ee0617f67e96750c
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-13 10:25:50 +0200

    add sout in constructors to verify injection construction

--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/CricketCoach.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/CricketCoach.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Component;
 @Component // this annotation marks the class as a Spring Bean for dependency injection
 public class CricketCoach implements Coach {
 
+    public CricketCoach () {
+        System.out.println("In constructor: " + getClass().getSimpleName()); // Will display name of class in console if injected
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/TrackCoach.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/TrackCoach.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary // This sets this class as the primary to be injected into controller constructor
+@Primary // This sets this class as the first choice to be injected into controller constructor
 public class TrackCoach implements Coach{
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName()); // Will display name of class in console if injected
+    }
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k!";
