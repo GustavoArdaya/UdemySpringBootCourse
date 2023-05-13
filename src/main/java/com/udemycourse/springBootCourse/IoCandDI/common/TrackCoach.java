210aa8a076ae7f07e9d8d0c463d9478758b7511c
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-13 10:12:47 +0200

    learned to use @Primary annotation on classes to give higher priority for injection in case of multiple classes

--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/TrackCoach.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/TrackCoach.java
@@ -1,8 +1,10 @@
 package com.udemycourse.springBootCourse.IoCandDI.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // This sets this class as the primary to be injected into controller constructor
 public class TrackCoach implements Coach{
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/rest/DemoController.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/rest/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
     @Autowired  // Tells Spring to inject a dependency. Annotation is optional if you only have one constructor
     public DemoController(@Qualifier("baseballCoach") Coach theCoach) { // with @Qualifier annotation I specify which coach to use
         myCoach = theCoach;                                             // note that the name is same as class, but beginning with lower case
-    }
+    }                                                                   // You can skip this by @Primary annotation in classes. Qualifier has higher priority
 
     // Setter injection: // uses setMethod instead of constructor
 //    @Autowired // even though its a setter (it could have any name) autowired allows program to use method automatically
