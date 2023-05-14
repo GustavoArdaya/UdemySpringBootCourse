519d69b2097aabb62623fd5ef5dd64e8b4d716eb
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-14 09:55:55 +0200

    add PostConstruct and PreDestroy annotations on baseball coach class

--- src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/BaseballCoach.java
+++ src/main/java/com/udemycourse/springBootCourse/IoCandDI/common/BaseballCoach.java
@@ -1,13 +1,30 @@
 package com.udemycourse.springBootCourse.IoCandDI.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component  //Don't forget to add annotation!
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON) //"Prototype" will create new instances each time the bean is injected
 public class BaseballCoach implements Coach{
 
     public BaseballCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName()); // Will display name of class in console if injected
     }
+
+    // init method
+    @PostConstruct
+    public void atStartup() {
+        System.out.println("This code executes during " + getClass().getSimpleName() + " initialization");
+    }
+
+    @PreDestroy
+    public void atDestroy() {
+        System.out.println("This code executes during " + getClass().getSimpleName() + " destruction");
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes in batting practice";
